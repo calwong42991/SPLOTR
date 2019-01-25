9eefc2b6f1cbc5e522c45ba11cc8c19085edd962
Calvin Wong <calwong42991@gmail.com>
2019-01-24 23:14:11 -0800

    edited webpack config file

---
+++ React-client/config/webpack.base.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => {
+  const {
+    PLATFORM,
+    VERSION
+  } = env;
+
+  const APP_DIR = path.resolve(__dirname, '../src/index.jsx');
+
+  return merge([{
+    entry: ['@babel/polyfill', APP_DIR],
+    module: {
+      rules: [{
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader'
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './React-Client/dist/index.html',
+        filename: './index.html'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.VERSION': JSON.stringify(env.VERSION),
+        'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+      }),
+      new CopyWebpackPlugin([{
+        from: 'React-Client/dist/static'
+      }]),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  }])
+}
---
+++ React-client/config/webpack.prod.config.js
@@ -0,0 +1,26 @@
+var merge = require('webpack-merge');
+
+// Plugins
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var Visualizer = require('webpack-visualizer-plugin');
+
+var baseConfig = require('./webpack.base.config');
+
+const prodConfiguration = env => {
+  return merge([
+    {
+      optimization: {
+        minimizer: [new UglifyJsPlugin()],
+      },
+      plugins: [
+        new OptimizeCssAssetsPlugin(),
+        new Visualizer({ filename: './statistics.html' })
+      ],
+    },
+  ]);
+}
+
+module.exports = env => {
+  return merge(baseConfig(env), prodConfiguration(env));
+}
